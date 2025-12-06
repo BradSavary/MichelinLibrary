40cd9e115b3d0163b17e3634e4689e578c42bcd7
BradSavary <brad.savary@etu.unilim.fr>
2025-12-06 13:36:13 +0100

    fix sidebar au scroll

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -31,7 +31,7 @@ export default function Layout({ children }: LayoutProps) {
       <aside
         className={`
           fixed inset-y-0 left-0 z-50 w-64 bg-white border-r border-gray-200
-          transform transition-transform duration-300 ease-in-out lg:translate-x-0 lg:static lg:inset-auto
+          transform transition-transform duration-300 ease-in-out lg:translate-x-0
           ${sidebarOpen ? 'translate-x-0' : '-translate-x-full'}
         `}
       >
@@ -79,7 +79,7 @@ export default function Layout({ children }: LayoutProps) {
       </aside>
 
       {/* Contenu principal */}
-      <div className="flex-1 flex flex-col min-w-0">
+      <div className="flex-1 flex flex-col min-w-0 lg:ml-64">
         {/* Header mobile avec burger */}
         <header className="lg:hidden bg-white border-b border-gray-200 sticky top-0 z-30">
           <div className="flex items-center justify-between px-4 py-4">
